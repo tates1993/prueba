a5d53430f16ac2a1981b6438fb980e6d09c674b1
Medac <Medac@DESKTOP-ILA73PP>
2020-01-09 14:43:26 +0100

    cambio

--- prueba1/src/prueba1/prueba.java
+++ prueba1/src/prueba1/prueba.java
@@ -4,7 +4,7 @@ public class prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola mundo");
+System.out.println("hola mundo edkgnvrkegn ");
 	}
 
 }
